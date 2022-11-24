eaa0ff6d397a113d7a50da7f6f12eaeeb5bed6ae
riku_i_i_i <b203r018@s.kochi-u.ac.jp>
2022-11-24 17:32:35 +0900

    2022/11/24

---
+++ moku1.py
@@ -0,0 +1,10 @@
+import gym
+
+env = gym.make('CartPole-v0')    # Cartpole定義
+env.reset()    # Cartpoleの状態初期化
+
+for i in range(100):
+    env.render()    # Cartpoleのアニメーション
+    observation, reward, done, info,_score = env.step(env.action_space.sample())  # カートを動かし、結果を返す
+    print("Step:",i,done,"Reward:",reward,"Obs:",observation)
+    
